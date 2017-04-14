fefd88e20a87e01fd97a465cab6b56252910d174
Samiul A. Chowdhury <mac151830@utdallas.edu>
2017-04-14 16:10:22 -0500

    Removed Appbar searchwidget using autocompletetextview instead.

--- android/SparkNav/app/src/main/java/edu/utdallas/emse/sparknav/MainActivity.java
+++ android/SparkNav/app/src/main/java/edu/utdallas/emse/sparknav/MainActivity.java
@@ -83,33 +83,6 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main, menu);
 
-        // Get the SearchView and set the searchable configuration
-        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
-
-        // Assumes current activity is the searchable activity
-        //searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-
-        // Current activity is not the searchable activity
-        ComponentName componentName = new ComponentName(getApplicationContext(), SearchableActivity.class);
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName));
-        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
-
-        searchView.setOnQueryTextListener(
-                new SearchView.OnQueryTextListener() {
-                    @Override
-                    public boolean onQueryTextChange(String s) {
-                        // Text has changed apply filtering?
-                        return false;
-                    }
-
-                    @Override
-                    public boolean onQueryTextSubmit(String s) {
-                        // perform final search
-                        return false;
-                    }
-                }
-        );
         return true;
     }
 
